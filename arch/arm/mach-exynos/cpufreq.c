759cd0f4ccc10aceac6f2d356f89981ef0f99236
franciscofranco <franciscofranco.1990@gmail.com>
2012-06-01 12:51:58 +0100

    cpufreq.c: export available frequencies to the userspace

--- arch/arm/mach-exynos/cpufreq.c
+++ arch/arm/mach-exynos/cpufreq.c
@@ -708,12 +708,18 @@ static struct notifier_block exynos_cpufreq_reboot_notifier = {
 	.notifier_call = exynos_cpufreq_reboot_notifier_call,
 };
 
+static struct freq_attr *exynos_cpufreq_attr[] = {
+	&cpufreq_freq_attr_scaling_available_freqs,
+	NULL,
+};
+
 static struct cpufreq_driver exynos_driver = {
 	.flags		= CPUFREQ_STICKY,
 	.verify		= exynos_verify_speed,
 	.target		= exynos_target,
 	.get		= exynos_getspeed,
 	.init		= exynos_cpufreq_cpu_init,
+	.attr		= exynos_cpufreq_attr,
 	.name		= "exynos_cpufreq",
 #ifdef CONFIG_PM
 	.suspend	= exynos_cpufreq_suspend,
